196a746f6bf44badda0da47d3b6fb133bdc2ec03
Rodion Kadyumov <krrkes@gmail.com>
2023-03-26 14:14:11 +0500

    На проверку

---
+++ src/test/java/ru/netoloy/javaclo_1_spring_boot/ApplicationgTests.java
@@ -0,0 +1,13 @@
+package ru.netoloy.javaclo_1_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationgTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
